387805e745155e4c244db9c3735357b0f984de4a
RISHITA NAINWAL <nainwalrishita87@gmail.com>
2026-08-08 22:32:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (99.78%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& arr) {
         int n = arr.size();
-
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                arr[i][j] += min(arr[i + 1][j],
-                                 arr[i + 1][j + 1]);
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                arr[i][j]+=min(arr[i+1][j],arr[i+1][j+1]);
             }
         }
-
-        return arr[0][0];
-    }
+            return arr[0][0];
+        }
 };
