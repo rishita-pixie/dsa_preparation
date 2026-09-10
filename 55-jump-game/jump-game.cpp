3b57e7ac3bd5066d2feacfc3a45ed5fb771fbe38
RISHITA NAINWAL <nainwalrishita87@gmail.com>
2026-09-11 01:00:57 +0530

    Time: 4 ms (21.53%) | Memory: 52.4 MB (25.86%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-int t[10002];
-bool solve(int i,vector<int>& arr){
-    int n=arr.size();
-    if(t[i]!=-1) return t[i];
-    if(i==n-1) return t[i]=true;
-    for(int j=1;j<=arr[i]&&i+j<n;j++){
-        if(solve(i+j,arr)) return t[i]=true;
-    }
-    return t[i]=false;
-}
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        memset(t,-1,sizeof(t));
-        return solve(0,nums);
+        //greedy
+        int  n=nums.size();
+          int maxi=0;
+          //dry run krke check kr
+        for(int i=0;i<n;i++){
+            if(i>maxi) return false;
+            maxi=max(maxi,i+nums[i]);
+        }
+        return true;
     }
 };
