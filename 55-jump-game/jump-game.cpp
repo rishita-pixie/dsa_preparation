ed343dd51444e787d10db234c8c3ae651dc68166
RISHITA NAINWAL <nainwalrishita87@gmail.com>
2026-09-11 00:56:31 +0530

    Time: 474 ms (10.90%) | Memory: 53.4 MB (11.60%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+int t[10002];
+bool solve(int i,vector<int>& arr){
+    int n=arr.size();
+    if(t[i]!=-1) return t[i];
+    if(i==n-1) return true;
+    for(int j=1;j<=arr[i]&&i+j<n;j++){
+        if(solve(i+j,arr)) return true;
+    }
+    return t[i]=false;
+}
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        memset(t,-1,sizeof(t));
+        return solve(0,nums);
+    }
+};
