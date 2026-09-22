393bd55e3b53bd1b5daf55b9a43f01b2dcc9702c
RISHITA NAINWAL <nainwalrishita87@gmail.com>
2026-09-22 21:09:14 +0530

    Time: 41 ms (44.42%) | Memory: 13.8 MB (96.17%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+int t[10004][13];
+int solve(vector<int>&arr,int a,int i){
+     int n=arr.size();
+if(a==0) return t[a][i]=0;
+if(t[a][i]!=-1) return t[a][i];
+if(i==n) return 1e9;
+int pick=1e9;
+if(arr[i]<=a) pick=1+solve(arr,a-arr[i],i);
+int notpick=solve(arr,a,i+1);
+return t[a][i]=min(pick,notpick);
+
+}
+    int coinChange(vector<int>&arr , int a) {
+        int n=arr.size();
+        memset(t,-1,sizeof(t));
+        int ans=solve(arr,a,0);
+    
+    if(ans==1e9) return -1;
+    return ans;}
+};
